6a047efcf4947f216a41fff7e236e2130035c024
TomFrankNY <tomcichra@gmail.com>
2022-12-07 15:23:32 -0300

    magic checkboxes on all items

--- form.php
+++ form.php
@@ -1,8 +1,30 @@
 <html>
-<body>
-<?php
-echo "the value of the checkbox is" $_POST["checkbox_name"] ;
+<body>  
+the value of the checkbox is: 
+<?php   
+if (isset($_POST['checkbox_name'])) {
+    echo $_REQUEST['checkbox_name'];
+
+    $checkbox_name = $_POST['checkbox_name'];
+
+    $query = "INSERT INTO checkbox (name) VALUES ('$checkbox_name',)";
+    $query_run = mysqli_query($con, $query);
+    
+    if($query_run)
+    {
+        $_SESSION['status'] = "Inserted Successfully";
+        header("Location: form.php");
+    }
+    else
+    {
+        $_SESSION['status'] = "Not Inserted";
+        header("Location: form.php");
+    }
+}
+
+
 
 ?>
+<br>
 </body>
 </html>
--- gallery.php
+++ gallery.php
@@ -6,29 +6,36 @@ function gallery($mysqli)
     if ($result->num_rows > 0) {
         //output data of each row
         while ($row = $result->fetch_assoc()) {
+            
+            ?>
+            <label for="checkbox">First name:</label>
+                <input type="checkbox" name="checkbox_name" class= "delete-checkbox">
+<?php
             echo "The name is: " . $row["name"] . "<br>" .
                 "The price is: " . $row["price"] . "<br>" .
                 "The type is: " . $row["type_id"] . "<br>";
         }
     }
-    echo 'Success: A proper connection to MySQL was made.';
-    echo '<br>';
+  
+    ?>
+
+    <html>
+    <head>
+        <title>Webslesson Tutorial | Simple PHP Mysql Shopping Cart</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div>
+            <form action="form.php" method ="post>
+                <input type="submit">
+            </form>
+        </div>
+    </body>
+</html>
+
+<?php
 }
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Webslesson Tutorial | Simple PHP Mysql Shopping Cart</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-</head>
-<body>
-    <div>
-        <form action="form.php" method ="post>
-            <input type="checkbox" name="checkbox_name" value="checkox_value" class= "delete-checkbox">
-            <input type="submit">
-        </form>
-    </div>
-</body>
-</html>
